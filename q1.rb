db4ce68fc326f7b8656c83c4041ded7445200cf5
soulchild <axelsince1993@gmail.com>
2014-03-26 16:03:08 +0800

    Question 1

---
+++ q1.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby -w
+
+puts "Project Euler question 1";
+
+$sum = 0;
+
+#below 1000 does not includes 1000
+for i in 1..999
+	if i%3 == 0 and i%5!=0
+		$sum+=i;
+
+	elsif i%3 !=0 and i%5==0
+		$sum+=i;
+
+	elsif i%3==0 and i%5==0
+		$sum+=i;
+	end
+end
+
+puts $sum;
+#233168
