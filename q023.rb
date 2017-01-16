bbe504c9acc6f895f9794afee6465f7008c21c18
Axel Kee <axelshaded@gmail.com>
2017-01-16 12:54:44 +0800

    Added solution of q23
    Added solution of q23

--- q023.rb
+++ q023.rb
@@ -9,16 +9,14 @@ class Numeric
 
 end
 
-# hash table to store sum of divisor
+# hash table to store sum of divisor, abundant number and sum of two abudant numbers
 $sumOfDivisors = Hash.new
 $abundantNumbersHash = Hash.new
 $sumOfAbundantNumbers = Hash.new
 
 $ans = 0
 
-# prevent duplicate number to added, record previously added number to this hash
-$added = Hash.new
-
+# perform sum of divisor of a number n, and add it to hash if it is abudant number
 def sumOfDivisor(n)
 
 	if $sumOfDivisors.has_key?(n)
@@ -52,21 +50,44 @@ def sumOfDivisor(n)
 end
 
 # 28112 because 28123+1-12
-#compute all abudant numbers below 28112
+# compute all abudant numbers below 28112
 for a in 1..28112
 	sumOfDivisor(a)
 end
 
-abundantNumberCount = $abundantNumbersHash.count
+# compute the sum of two abudant numbers
+$abundantNumbersHash.each_with_index do |(key, value), index|
+	$abundantNumbersHash.each_with_index do |(key2, value2), index2|
 
-$abundantNumbersHash.each do |abudantNumberOne|
-	$abundantNumbersHash.each do |abudantNumberTwo|
-		abundantNumberSum = abudantNumberOne + abudantNumberTwo
+		abundantNumberSum = key + key2
+
+		# ignore if the sum is larger than 28123 since numbers above 28123 
+		# can always be written in form of sum of two abundant number
+		if abundantNumberSum > 28123
+			next
+		end
+
+		# ignore if previously computed
 		if $sumOfAbundantNumbers.has_key?(abundantNumberSum)
 			next
 		end
+
+		$sumOfAbundantNumbers[abundantNumberSum] = 1
 	end
 end
 
+# compute the answer
+for a in 1..28123
+
+	# move on if the number can be written as sum of two abudant numbers
+	if $sumOfAbundantNumbers.has_key?(a)
+		next
+	end
+
+	# add to the answer sum otherwise
+	$ans += a
+end
+
+puts $ans
 
 
